b9c9d27a369b0747f2985de4884f6cca93da3d2f
Ron Savage <ron@savage.net.au>
2010-08-24 05:00:08 +0000

    Import of RSAVAGE/DBIx-HTML-PopupRadio-1.16 from CPAN.
    gitpan-cpan-distribution: DBIx-HTML-PopupRadio
    gitpan-cpan-version:      1.16
    gitpan-cpan-path:         RSAVAGE/DBIx-HTML-PopupRadio-1.16.tgz
    gitpan-cpan-author:       RSAVAGE
    gitpan-cpan-maturity:     released

--- lib/DBIx/HTML/PopupRadio.pm
+++ lib/DBIx/HTML/PopupRadio.pm
@@ -50,7 +50,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(
 
 );
-our $VERSION = '1.13';
+our $VERSION = '1.16';
 
 # -----------------------------------------------
 
@@ -597,8 +597,7 @@ Home page: http://savage.net.au/index.html
 
 =head1 Copyright
 
-Australian copyright (c) 2002, Ron Savage. All rights reserved.
-
+Australian copyright (c) 2002, Ron Savage.
 	All Programs of mine are 'OSI Certified Open Source Software';
 	you can redistribute them and/or modify them under the terms of
 	The Artistic License, a copy of which is available at:
